df37a1623bfc334a5144b41e8d7b32bdc63aceee
Jesus Cueto <u201212068@upc.edu.pe>
2017-09-28 19:00:28 -0500

    Sign Up

---
+++ PetHealth/app/src/main/java/pe/edu/upc/pethealth/activities/SingUpActivity.java
@@ -0,0 +1,29 @@
+package pe.edu.upc.pethealth.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import pe.edu.upc.pethealth.R;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    Button doneButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+        doneButton = (Button) findViewById(R.id.doneButton);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                Intent intent = new Intent(context,LoginActivity.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+}
