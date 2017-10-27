842a999d745e20fd76963b834184c040709f6a98
Jesus Cueto <u201212068@upc.edu.pe>
2017-10-27 16:53:18 -0500

    User and Login

--- PetHealth/app/src/main/java/pe/edu/upc/pethealth/activities/StartActivity.java
+++ PetHealth/app/src/main/java/pe/edu/upc/pethealth/activities/StartActivity.java
@@ -3,18 +3,27 @@ package pe.edu.upc.pethealth.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import pe.edu.upc.pethealth.R;
+import pe.edu.upc.pethealth.models.User;
+import pe.edu.upc.pethealth.network.PetHealthApiService;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -23,6 +32,7 @@ public class StartActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private Button signInButton;
     private Button signUpBtutton;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,7 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
+        user = new User();
         logoImageView = (ImageView) findViewById(R.id.logoImageView);
         userEditText = (EditText) findViewById(R.id.emailEditText);
         passwordEditText = (EditText) findViewById(R.id.passwordEditTextView);
@@ -64,6 +75,7 @@ public class StartActivity extends AppCompatActivity {
         // Reset errors.
         userEditText.setError(null);
         passwordEditText.setError(null);
+        final Context context = this;
 
         // Store values at the time of the login attempt.
         String email = userEditText.getText().toString();
@@ -73,8 +85,8 @@ public class StartActivity extends AppCompatActivity {
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            passwordEditText.setError(getString(R.string.error_invalid_password));
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError(getString(R.string.error_field_required));
             focusView = passwordEditText;
             cancel = true;
         }
@@ -84,10 +96,6 @@ public class StartActivity extends AppCompatActivity {
             userEditText.setError(getString(R.string.error_field_required));
             focusView = userEditText;
             cancel = true;
-        } else if (!isEmailValid(email)) {
-            userEditText.setError(getString(R.string.error_invalid_email));
-            focusView = userEditText;
-            cancel = true;
         }
 
         if (cancel) {
@@ -95,16 +103,41 @@ public class StartActivity extends AppCompatActivity {
             // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            Context context = this;
-            Intent intent = new Intent(context,MainActivity.class);
-            context.startActivity(intent);
+
+            AndroidNetworking.post(PetHealthApiService.LOGIN_URL)
+                    .addBodyParameter("username", email)
+                    .addBodyParameter("password", password)
+                    .setTag(getString(R.string.app_name))
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsJSONObject(new JSONObjectRequestListener() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // do anything with response
+                            try {
+                                if ("success".equalsIgnoreCase(response.getString("status"))) {
+                                   // user = user.from(response.getJSONObject("userLog"));
+                                    Intent intent = new Intent(context, MainActivity.class);
+                                    //intent.putExtras(user.toBundle());
+                                    context.startActivity(intent);
+                                } else {
+                                    Log.d(getString(R.string.app_name), "User and password are incorrect");
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onError(ANError anError) {
+                            Log.d(getString(R.string.app_name), anError.getLocalizedMessage());
+                        }
+                    });
         }
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         if(email.equals("admin"))
             return true;
         else
