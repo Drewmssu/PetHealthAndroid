49a9bde85e506daab6589938f7fcc2acae821826
palvarezm <palvareze7@gmail.com>
2017-11-10 02:29:51 -0500

    Add spinner and some landscape views

--- PetHealth/app/src/main/java/pe/edu/upc/pethealth/activities/SignUpActivity.java
+++ PetHealth/app/src/main/java/pe/edu/upc/pethealth/activities/SignUpActivity.java
@@ -5,21 +5,43 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import pe.edu.upc.pethealth.R;
+import pe.edu.upc.pethealth.models.DocumentType;
+import pe.edu.upc.pethealth.network.PetHealthApiService;
 
 public class SignUpActivity extends AppCompatActivity {
 
     Button doneButton;
     EditText birthDateEditText;
     DatePickerDialog datePickerDialog;
+    RecyclerView documentTypeListRecyclerView;
+    List<DocumentType> documentTypeList;
+    DocumentType documentType;
+    String tag;
+    Spinner documentTypeSpinner;
 
+    //Crea el calendario para la vista con la fecha de hoy
     Calendar calendar = Calendar.getInstance();
     int yy = calendar.get(Calendar.YEAR);
     int mm = calendar.get(Calendar.MONTH);
@@ -28,6 +50,7 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+        documentTypeSpinner = (Spinner) findViewById(R.id.documentTypeSpinner);
         birthDateEditText = (EditText) findViewById(R.id.birthDateEditText);
         birthDateEditText.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -62,5 +85,45 @@ public class SignUpActivity extends AppCompatActivity {
                 context.startActivity(intent);
             }
         });
+
+        documentTypeList = new ArrayList<>();
+        documentType= DocumentType.from(getIntent().getExtras());
+        tag = "PetHealth";
+
+        updateDocumentTypeList();
+    }
+
+    private void updateDocumentTypeList(){
+        String _id = Integer.toString(documentType.getId());
+
+        AndroidNetworking.get(PetHealthApiService.DOCTYPE_URL)
+                .addQueryParameter("id", _id)
+                .addQueryParameter("name", documentType.getName())
+                .addQueryParameter("shortening", documentType.getShortening())
+                .setTag(tag)
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if("error".equalsIgnoreCase(response.getString("status"))){
+                                Log.d(tag,response.getString("message"));
+                                return;
+                            }
+                            documentTypeList = DocumentType.from(response.getJSONArray("documentTypeList"));
+
+                            //adapter setDocumentTypes(documentTypeList);
+                            //adapter notifyDataSetChanged();
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(tag, anError.getErrorBody());
+                    }
+                });
     }
 }
--- PetHealth/app/src/main/java/pe/edu/upc/pethealth/network/PetHealthApiService.java
+++ PetHealth/app/src/main/java/pe/edu/upc/pethealth/network/PetHealthApiService.java
@@ -8,4 +8,5 @@ public class PetHealthApiService {
     public static String PET_URL = "http://pethealth.azurewebsites.net";
     public static String TIP_URL = "http://pethealth.azurewebsites.net/tips";
     public static String LOGIN_URL = "http://pethealth.azurewebsites.net/user/login";
+    public static String DOCTYPE_URL = "http://pethealth.azurewebsites.net/documentType";
 }
