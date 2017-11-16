36f61156b54efb6990cec145e46fd18a4291e66f
DESKTOP-HB0C1U8\Usuario <gonzalo_cirilo@hotmail.com>
2017-11-15 20:23:25 -0500

    loading view for Images added

--- PetHealth/app/src/main/java/pe/edu/upc/pethealth/adapters/MyPetAdapters.java
+++ PetHealth/app/src/main/java/pe/edu/upc/pethealth/adapters/MyPetAdapters.java
@@ -2,6 +2,7 @@ package pe.edu.upc.pethealth.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,12 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.BitmapRequestListener;
 import com.androidnetworking.widget.ANImageView;
+import com.wang.avi.AVLoadingIndicatorView;
 
 import java.util.List;
 
@@ -40,11 +46,11 @@ public class MyPetAdapters extends RecyclerView.Adapter<MyPetAdapters.ViewHolder
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         final MyPet myPet = myPets.get(position);
+        holder.loadingIndicatorView.setVisibility(View.VISIBLE);
+        holder.petANImageView.setVisibility(View.INVISIBLE);
         holder.petNameTextView.setText(myPets.get(position).getName());
         holder.petDescriptionTextView.setText(myPets.get(position).getDescription());
-        holder.petANImageView.setImageUrl(myPets.get(position).getImage());
-        holder.petANImageView.setDefaultImageResId(R.mipmap.ic_launcher);
-        holder.petANImageView.setErrorImageResId(R.mipmap.ic_launcher);
+        loadImage(myPets.get(position).getImage(),holder.petANImageView,holder.loadingIndicatorView);
         holder.moreTextView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -73,6 +79,28 @@ public class MyPetAdapters extends RecyclerView.Adapter<MyPetAdapters.ViewHolder
 
     }
 
+    private void loadImage(String imageUrl, final ImageView imageView, final AVLoadingIndicatorView loadingIndicatorView){
+        AndroidNetworking.get(imageUrl)
+                .setTag("imageRequest")
+                .setPriority(Priority.MEDIUM)
+                .setBitmapMaxHeight(240)
+                .setBitmapMaxWidth(240)
+                .build()
+                .getAsBitmap(new BitmapRequestListener() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        imageView.setImageBitmap(response);
+                        imageView.setVisibility(View.VISIBLE);
+                        loadingIndicatorView.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        System.out.println(anError.toString());
+                    }
+                });
+    }
+
     @Override
     public int getItemCount() {
         return myPets.size();
@@ -89,18 +117,20 @@ public class MyPetAdapters extends RecyclerView.Adapter<MyPetAdapters.ViewHolder
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView petNameTextView;
-        ANImageView petANImageView;
+        ImageView petANImageView;
         TextView petDescriptionTextView;
         TextView moreTextView;
         ImageButton clinicHystoryImageButton;
+        AVLoadingIndicatorView loadingIndicatorView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             petNameTextView = (TextView) itemView.findViewById(R.id.petNameTextView);
-            petANImageView = (ANImageView) itemView.findViewById(R.id.myPetImageView);
+            petANImageView = (ImageView) itemView.findViewById(R.id.myPetImageView);
             petDescriptionTextView = (TextView) itemView.findViewById(R.id.myPetDescriptionTextView);
             moreTextView = (TextView) itemView.findViewById(R.id.moreTextView);
             clinicHystoryImageButton = (ImageButton) itemView.findViewById(R.id.historyClinicImageButton);
+            loadingIndicatorView = (AVLoadingIndicatorView) itemView.findViewById(R.id.avi);
         }
     }
 }
